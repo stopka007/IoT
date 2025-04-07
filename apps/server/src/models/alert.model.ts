ea163f0083c4bf589364f0bea1463b3d7858bb61
c1op <c1op@proton.me>
2025-04-08 00:00:05 +0200

    feat: use id_device in alerts

--- apps/server/src/models/alert.model.ts
+++ apps/server/src/models/alert.model.ts
@@ -1,17 +1,15 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IAlert extends Document {
-  id_patient: string;
+  id_device: string;
   timestamp: Date;
   status: "open" | "resolved";
-  message: string;
 }
 
 const AlertSchema: Schema = new Schema({
-  id_patient: { type: String, required: true, ref: "Patient" },
+  id_device: { type: String, required: true, ref: "Device" },
   timestamp: { type: Date, default: Date.now },
   status: { type: String, enum: ["open", "resolved"], default: "open" },
-  message: { type: String, required: true },
 });
 
 export default mongoose.model<IAlert>("Alert", AlertSchema);
