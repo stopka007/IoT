320b5bcb81db64035a230fcc66b4690b157cc6a0
c1op <c1op@proton.me>
2025-04-07 23:23:26 +0200

    feat: add alerts field to device model

--- apps/server/src/models/device.model.ts
+++ apps/server/src/models/device.model.ts
@@ -6,6 +6,7 @@ export interface IDevice extends Document {
   id_patient: string;
   battery_level: number;
   help_needed: boolean;
+  alerts: mongoose.Types.ObjectId[];
 }
 
 const DeviceSchema = new Schema<IDevice>({
@@ -14,6 +15,7 @@ const DeviceSchema = new Schema<IDevice>({
   id_patient: { type: String, required: true },
   battery_level: { type: Number, required: true },
   help_needed: { type: Boolean, required: true },
+  alerts: [{ type: Schema.Types.ObjectId, ref: "Alert" }],
 });
 
 export const Device = mongoose.model<IDevice>("Device", DeviceSchema);
