96ae655ee9b4691be109fa863df29485bfff0282
c1op <c1op@proton.me>
2025-04-27 15:59:49 +0200

    feat: add user

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -5,6 +5,7 @@ import deviceRoutes from "./device.route";
 import exampleRoutes from "./example.route";
 import patientRoutes from "./patient.route";
 import roomRoutes from "./room.route";
+import userRoutes from "./user.route";
 
 export const registerRoutes = (server: FastifyInstance) => {
   server.register(patientRoutes, { prefix: "/api/patients" });
@@ -12,4 +13,5 @@ export const registerRoutes = (server: FastifyInstance) => {
   server.register(alertRoutes, { prefix: "/api/alerts" });
   server.register(deviceRoutes, { prefix: "/api/devices" });
   server.register(roomRoutes, { prefix: "/api/rooms" });
+  server.register(userRoutes, { prefix: "/api/users" });
 };
