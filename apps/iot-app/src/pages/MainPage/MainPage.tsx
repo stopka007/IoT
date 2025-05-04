e98a8b881777bc20c1db7c7e4403128ac05b70a1
Ondřej Parchomenko <parchomenkoondra@seznam.cz>
2025-05-04 21:30:16 +0200

    přidání pořádného switche

--- apps/iot-app/src/pages/MainPage/MainPage.tsx
+++ apps/iot-app/src/pages/MainPage/MainPage.tsx
@@ -81,6 +81,17 @@ export default function MainPage() {
             >
               {theme === "light" ? "Tmavý režim" : "Světlý režim"}
             </button>
+            <div className="flex items-center space-x-2">
+              <label className="inline-flex items-center cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="sr-only peer"
+                  checked={showDetailedView}
+                  onChange={() => setShowDetailedView(prev => !prev)}
+                />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+              </label>
+            </div>
             <div className="relative">
               <button
                 onClick={() => setIsDropdownOpen(prev => !prev)}
@@ -125,15 +136,6 @@ export default function MainPage() {
                   </button>
                 </div>
               )}
-            </div>
-            <div className="flex items-center space-x-2">
-              <label className="text-sm text-gray-200">Zobrazit přehled</label>
-              <input
-                type="checkbox"
-                checked={showDetailedView}
-                onChange={() => setShowDetailedView(prev => !prev)}
-                className="form-checkbox h-4 w-4 text-blue-600"
-              />
             </div>
           </div>
         </header>
